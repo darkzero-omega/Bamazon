eac6b2fdda8f28e2816914141e46e13e30e58231
darkzero_omega <darkzero_omega2003@yahoo.com>
2018-05-31 03:00:41 -0400

    updated js file and debugged errors

--- assets/javascript/BamazonCustomer.js
+++ assets/javascript/BamazonCustomer.js
@@ -1,13 +1,13 @@
 //--Requiring Dependencies--//
 var mysql = require('mysql');
-var password = require('./password.js');
+//var password = require('./password.js');
 var prompt = require('prompt');
 
 //--Setting up connection tp mySQL--//
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: password,
+    password: "Ragnarock_1",
     database: 'bamazon'
 });
 
@@ -27,7 +27,7 @@ console.log("");
 connection.query('SELECT * FROM products', function (err, rows) {
             if (err) throw err;
             //---Log all the items for sale in the console--//
-            for (var i = 0; < rows.length; i++) {
+            for (var i = 0; i < rows.length; i++) {
 
                 console.log("Item ID:" + rows[i].ItemID + rows[i].product_name + "Price $" + rows[i].Price);
             };
@@ -50,30 +50,31 @@ connection.query('SELECT * FROM products', function (err, rows) {
             }
 
 
-            //--Ask the user what they want to buy and how much.--//
+        //--Ask the user what they want to buy and how much.--//
 
-            prompt.get(schema, function (err, result) {
-                //--If the user wants more than we have in stock the we tell them we dont have enough--//
-                if (rows[result.itemid - 1].stockQuantity < result.quantity) {
-                    confirm.log("We don't have that much stock bro");
+        prompt.get(schema, function (err, result) {
+            //--If the user wants more than we have in stock the we tell them we dont have enough--//
+            if (rows[result.itemid - 1].stockQuantity < result.quantity) {
+                confirm.log("We don't have that much stock bro");
 
-                    //--If we DO have enough stock--//
-                } else {
-                    //--The total amount for the user--//
-                    var orderPrice = (row[result.itemid - 1].Price * result.quantity);
-                    var department = rows[result.itemid - 1].DepartmentName;
-                    console.log("We'll sell at a high price!!");
-                    console.log("Your oder costs:$" + orderPrice);
-                    console.log("Thank you for buying from Honest Bill  and Joel's Shady Shack of Knicknacks and Lemons!!");
+                //--If we DO have enough stock--//
+            } else {
+                //--The total amount for the user--//
+                var orderPrice = (row[result.itemid - 1].Price * result.quantity);
+                var department = rows[result.itemid - 1].DepartmentName;
+                console.log("We'll sell at a high price!!");
+                console.log("Your oder costs:$" + orderPrice);
+                console.log("Thank you for buying from Honest Bill  and Joel's Shady Shack of Knicknacks and Lemons!!");
 
-                    //--Update the total sales for the department--//
-                    connection.query('SELECT * FROM Departments', function (err, rows
+                //--Update the total sales for the department--//
+                connection.query('SELECT * FROM Departments', function (err, rows
 
-                        ) {
-                            connection.query('UPDATE Departments SET TotalSales = TotalSales +' + 'WHERE ItemID=' + result.itemid + ',', function (err, res) {
-                                if (err) throw err;
-                            });
-                        });
+                ) {
+                    connection.query('UPDATE Departments SET TotalSales = TotalSales +' + 'WHERE ItemID=' + result.itemid + ',', function (err, res) {
+                        if (err) throw err;
+                    });
+                });
 
-                    }
-            });
+            }
+        });
+    })
